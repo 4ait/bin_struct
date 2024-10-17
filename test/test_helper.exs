74653acabc74e169e9047583c7185f7018e01cec
Ridtt <ridtt.tz@gmail.com>
2024-10-17 21:31:02 +1000

    up todos

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,7 @@
 ExUnit.start()
+#todo implement tests for all list bounds variants
+#start with lib/macro/preprocess/remap_list_of.ex
+
+#todo implement tests for all dynamic callbacks
+#todo implement tests options interfaces
+#todo implement tests for virtual fields
