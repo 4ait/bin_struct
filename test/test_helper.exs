f556ac74ef9ccaba04c61fb34d6d17ed834b5c96
Ridtt <ridtt.tz@gmail.com>
2024-10-18 21:37:45 +1000

    tests implementation checklist checked

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,5 @@
 ExUnit.start()
-#todo implement tests for all list bounds variants
-#start with lib/macro/preprocess/remap_list_of.ex
+#implemented: tests for all list bounds variants
 
 #todo implement tests for all dynamic callbacks
 #todo implement tests options interfaces
