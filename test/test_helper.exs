553692b73a0690d94615801a6d9f36425fcc41ba
Ridtt <ridtt.tz@gmail.com>
2024-10-18 21:43:20 +1000

    changed checklist for tests to implement

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,11 @@
 ExUnit.start()
 #implemented: tests for all list bounds variants
 
-#todo implement tests for all dynamic callbacks
+#todo implement tests optional fields (dynamic or optional tail)
+#todo implement tests for dynamic validation
+#todo implement tests for builder
+#todo implement tests for read/write virtual system
+
+#check callbacks list here lib/macro/preprocess/remap_callback_options.ex
+
 #todo implement tests options interfaces
-#todo implement tests for virtual fields
