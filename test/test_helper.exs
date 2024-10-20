dd4a0e5084a2030bb28f3920fe3802693a909aa1
Ridtt <ridtt.tz@gmail.com>
2024-10-20 23:30:51 +1000

    todos

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -6,11 +6,10 @@ ExUnit.start()
 #implemented: implement tests for read/write virtual system
 #implemented: tests for options/option interfaces
 
-#todolater add more detailed examples of uses virtual field system in practice
+#todolowprio add exceptions and warnings for unsupported options where it will be unused
+#todolowprio ... ( like take_while_by will be unused if list already have bounds, etc)
 
-#todo add exceptions and warnings for unsupported options where it will be unused
-#todo ... ( like take_while_by will be unused if list already have bounds, etc)
+#implemented: BinStructCustomType with custom_type_arguments
 
-#todo add benchmarks
-#todo add BinStructCustomType with custom_type_arguments
 #todo type managed unmanaged table
+#todolater add more detailed examples of uses virtual field system in practice
