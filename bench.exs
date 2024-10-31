bdc8b0aab51a0172c9902ec0df1890208f58b5dd
Ridtt <ridtt.tz@gmail.com>
2024-10-31 12:27:10 +1000

    moved bench separate

--- bench.exs
+++ bench.exs
@@ -1,3 +1,65 @@
+defmodule BenchBinStructWithPrimitiveItemList do
+
+  use BinStruct
+
+  field :items, { :list_of, :uint32_be }, length: 1000
+
+end
+
+defmodule BenchBinStructWithDynamicPrimitiveItemList do
+
+  use BinStruct
+
+  register_callback &take_while_not_1000_items/1,
+                    items: :field
+
+  field :items, { :list_of, :uint32_be }, take_while_by: &take_while_not_1000_items/1
+
+  defp take_while_not_1000_items([1000 | _prev]), do: :halt
+  defp take_while_not_1000_items(_), do: :cont
+
+end
+
+defmodule BenchItem do
+
+  use BinStruct
+
+  field :value, :uint32_be
+
+end
+
+
+defmodule BenchBinStructWithStructItemList do
+
+  use BinStruct
+
+  field :items, { :list_of, BenchItem }, length: 1000
+
+end
+
+defmodule BenchBinStructWithStructDynamicItemList do
+
+  use BinStruct
+
+  register_callback &take_while_not_1000_items/1,
+                    items: :field
+
+  field :items, { :list_of, BenchItem }, take_while_by: &take_while_not_1000_items/1
+
+  defp take_while_not_1000_items([head | _prev]) do
+
+    %{ value: value } = BenchItem.decode(head)
+
+    case value do
+      1000 -> :halt
+      _ -> :cont
+    end
+
+  end
+
+end
+
+
 defmodule BinaryValuesBench do
 
   def benchmark() do
@@ -9,7 +71,7 @@ defmodule BinaryValuesBench do
           1..1000,
           <<>>,
           fn number, acc ->
-            acc <> <<number::16-big-unsigned>>
+            acc <> <<number::32-big-unsigned>>
           end
         )
     }
@@ -17,25 +79,23 @@ defmodule BinaryValuesBench do
     Benchee.run(
       %{
         "parse, decode, new struct, and dump BinStructWithPrimitiveItemList" => (fn bin ->
-          { :ok, parsed_struct, _rest } = BinStructWithPrimitiveItemList.parse(bin)
+          { :ok, parsed_struct, _rest } = BenchBinStructWithPrimitiveItemList.parse(bin)
         end),
 
         "parse, decode, new struct, and dump BinStructWithDynamicPrimitiveItemList" => (fn bin ->
-           { :ok, parsed_struct, _rest } = BinStructWithDynamicPrimitiveItemList.parse(bin)
+           { :ok, parsed_struct, _rest } = BenchBinStructWithDynamicPrimitiveItemList.parse(bin)
 
         end),
 
         "parse, decode, new struct, and dump BinStructWithStructItemList" => (fn bin ->
-            { :ok, parsed_struct, _rest } = BinStructWithStructItemList.parse(bin)
+            { :ok, parsed_struct, _rest } = BenchBinStructWithStructItemList.parse(bin)
 
                                                                               end),
 
         "parse, decode, new struct, and dump BinStructWithStructDynamicItemList" => (fn bin ->
-          { :ok, parsed_struct, _rest } = BinStructWithStructDynamicItemList.parse(bin)
+          { :ok, parsed_struct, _rest } = BenchBinStructWithStructDynamicItemList.parse(bin)
 
                                                                                              end),
-
-
 
       },
       inputs: inputs
