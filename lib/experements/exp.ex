060edbc972c202c1b3f5a9f6966113d760f1849d
Ridtt <ridtt.tz@gmail.com>
2024-11-06 02:53:10 +1000

    up sparse

--- lib/experements/exp.ex
+++ lib/experements/exp.ex
@@ -1,79 +1,15 @@
-defmodule Interface1 do
+defmodule TestStruct do
 
-  use BinStructOptionsInterface
-
-  register_options_interface do
-    register_option :o1
-  end
-
-end
-
-defmodule Interface2 do
-
-  use BinStructOptionsInterface
-
-  register_options_interface do
-    register_option :o2
-  end
-
-end
-
-defmodule Interface3 do
-
-  use BinStructOptionsInterface
-
-  register_options_interface do
-    register_option :o3
-  end
-
-end
-
-defmodule OptionsReceiver do
+  #todo implement node sparse algorithm, keep as much distance as we can not breaking connections rules
 
   use BinStruct
 
-  register_callback &length_by/3,
-                    o1: %{ type: :option, interface: Interface1 },
-                    o2: %{ type: :option, interface: Interface2 },
-                    o3: %{ type: :option, interface: Interface3 }
+  register_callback &length_by_a/1, a: :field
 
-  field :data, :binary, length_by: &length_by/3
+  field :a, :uint16_be
+  field :b, :binary, length_by: &length_by_a/1
+  field :c, :binary, length_by: &length_by_a/1
 
-  defp length_by(o1, o2, o3) do
-    o1 + o2 + o3
-  end
-
-
-end
-
-defmodule OptionsSender do
-
-
-  use BinStruct
-
-  register_callback &impl_options_interface_1/1, a1: :field
-  register_callback &impl_options_interface_2/1, a2: :field
-  register_callback &impl_options_interface_3/1, a3: :field
-
-  field :a1, :uint16_be
-  field :a2, :uint16_be
-  field :a3, :uint16_be
-
-  impl_interface Interface1, &impl_options_interface_1/1
-  impl_interface Interface2, &impl_options_interface_2/1
-  impl_interface Interface3, &impl_options_interface_3/1
-
-  defp impl_options_interface_1(a1), do: Interface1.option_o1(a1)
-  defp impl_options_interface_2(a2), do: Interface2.option_o2(a2)
-  defp impl_options_interface_3(a3), do: Interface3.option_o3(a3)
-
-end
-
-defmodule TransportHeader do
-
-  use BinStruct
-
-  field :options_sender, OptionsSender
-  field :options_receiver, OptionsReceiver
+  defp length_by_a(a), do: a
 
 end
