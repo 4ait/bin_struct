e3f08d89ff8cdfd057a6b8dcaa21222574652885
Ridtt <ridtt.tz@gmail.com>
2024-11-09 15:47:57 +1000

    fix

--- lib/bin_struct/enum_value_by_variant_name.ex
+++ lib/bin_struct/enum_value_by_variant_name.ex
@@ -2,11 +2,12 @@ defmodule BinStruct.EnumValueByVariantName do
 
   def find_enum_value_by_variant_name([], _variant_name), do: nil
 
-  def find_enum_value_by_variant_name([ { candidate_variant_value, candidate_variant_name } | _tail ], variant_name)
-      when candidate_variant_name == variant_name, do: candidate_variant_value
-
-  def find_enum_value_by_variant_name(enum_def, variant_name) do
-    read_enum_value(enum_def, variant_name)
+  def find_enum_value_by_variant_name([{candidate_value, candidate_name} | rest], variant_name) do
+    if candidate_name == variant_name do
+      candidate_value
+    else
+      find_enum_value_by_variant_name(rest, variant_name)
+    end
   end
 
 end
--- lib/bin_struct/enum_variant_name_by_value.ex
+++ lib/bin_struct/enum_variant_name_by_value.ex
@@ -2,11 +2,12 @@ defmodule BinStruct.EnumVariantNameByValue do
 
   def find_enum_variant_name_by_value([], _enum_value), do: nil
 
-  def find_enum_variant_name_by_value([ { candidate_variant_value, candidate_variant_name } | _tail ], enum_value)
-      when candidate_variant_value == enum_value, do: candidate_variant_name
-
-  def find_enum_variant_name_by_value(enum_def, enum_value) do
-    find_enum_variant_name_by_value(enum_def, enum_value)
+  def find_enum_variant_name_by_value([{candidate_value, candidate_name} | rest], enum_value) do
+    if candidate_value == enum_value do
+      candidate_name
+    else
+      find_enum_variant_name_by_value(rest, enum_value)
+    end
   end
 
 end
