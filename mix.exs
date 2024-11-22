40a12b860437d1b3d8781fffe48595e9ddfa364f
Ridtt <69139406+Ridtt@users.noreply.github.com>
2024-11-22 22:49:58 +1000

    Update mix.exs
    Exluded ssl app

--- mix.exs
+++ mix.exs
@@ -57,7 +57,7 @@ defmodule BinStruct.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :ssl]
+      extra_applications: [:logger]
     ]
   end
 
