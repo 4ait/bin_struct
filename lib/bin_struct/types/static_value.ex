c7c1006cd60a21f831ff5994f55c9c5392bbe18f
Ridtt <ridtt.tz@gmail.com>
2024-11-24 00:50:20 +1000

    docs for optional static value

--- lib/bin_struct/types/static_value.ex
+++ lib/bin_struct/types/static_value.ex
@@ -27,6 +27,55 @@ defmodule BinStruct.Types.StaticValue do
 
     ```
 
+  ## Optional
+
+    ```
+
+      iex> defmodule StructOptionalStaticValueNotPresent do
+      ...>   use BinStruct
+      ...>   field :value, <<1, 2, 3>>, optional: true
+      ...> end
+      ...>
+      ...> StructOptionalStaticValueNotPresent.new()
+      ...> |> StructOptionalStaticValueNotPresent.dump_binary()
+      ...> |> StructOptionalStaticValueNotPresent.parse_exact()
+      ...> |> then(fn {:ok, struct } -> struct end)
+      ...> |> StructOptionalStaticValueNotPresent.decode()
+      %{ value: nil }
+
+      iex> defmodule StructOptionalStaticValuePresent do
+      ...>   use BinStruct
+      ...>   field :value, <<1, 2, 3>>, optional: true
+      ...> end
+      ...>
+      ...> StructOptionalStaticValuePresent.new(value: :present)
+      ...> |> StructOptionalStaticValuePresent.dump_binary()
+      ...> |> StructOptionalStaticValuePresent.parse_exact()
+      ...> |> then(fn {:ok, struct } -> struct end)
+      ...> |> StructOptionalStaticValuePresent.decode()
+      %{ value: <<1, 2, 3>> }
+
+    ```
+
+  ## Inline bitstring
+
+    ```
+
+      iex> defmodule StructInlineBitstring do
+      ...>   use BinStruct
+      ...>   field :first, <<1::1, 1::1, 1::1, 1::1>>
+      ...>   field :second, <<0::1, 0::1, 0::1, 0::1>>
+      ...> end
+      ...>
+      ...> StructInlineBitstring.new()
+      ...> |> StructInlineBitstring.dump_binary()
+      ...> |> StructInlineBitstring.parse()
+      ...> |> then(fn {:ok, struct, _rest } -> struct end)
+      ...> |> StructInlineBitstring.decode()
+      %{ first: <<1::1, 1::1, 1::1, 1::1>>, second: <<0::1, 0::1, 0::1, 0::1>> }
+
+    ```
+
   ## Inline String.t
 
     ```
