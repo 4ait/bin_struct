74af8cd806c316b04a5e9616c2eef81f60a3d69d
Ridtt <ridtt.tz@gmail.com>
2024-11-26 19:40:36 +1000

    don't delete notice

--- lib/bin_struct/type_conversion/type_conversion_managed.ex
+++ lib/bin_struct/type_conversion/type_conversion_managed.ex
@@ -1,3 +1,6 @@
 defmodule BinStruct.TypeConversion.TypeConversionManaged do
   @moduledoc false
+
+  #dont delete this module, library is matching it by name
+
 end
--- lib/bin_struct/type_conversion/type_conversion_unspecified.ex
+++ lib/bin_struct/type_conversion/type_conversion_unspecified.ex
@@ -1,3 +1,6 @@
 defmodule BinStruct.TypeConversion.TypeConversionUnspecified do
   @moduledoc false
+
+  #dont delete this module, library is matching it by name
+
 end
